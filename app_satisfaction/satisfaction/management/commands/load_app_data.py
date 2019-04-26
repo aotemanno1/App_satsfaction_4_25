c929036f46077a060f5c2dbeedd5b51fab005d6e
aotemanno1 <aotemanno1@gmail.com>
2019-04-26 00:02:54 -0400

    Added two cols into DB, but 1st col will disappear

--- app_satisfaction/satisfaction/management/commands/load_app_data.py
+++ app_satisfaction/satisfaction/management/commands/load_app_data.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 
 ALREADY_LOADED_ERROR_MESSAGE = """
+----->>manage.py migrate --run-syncdb<<-----stupid command
 If you need to reload the app data from the CSV file,
 first delete the db.sqlite3 file to destroy the database.
 Then, run `python manage.py migrate` for a new empty
@@ -26,36 +27,38 @@ class Command(BaseCommand):
             return
 
         # for row in DictReader(open('C:\\Users\\wenjli\\Desktop\\Apps2Beta1.csv')):
-        path_to_file = "C:\\Users\\wenjli\\Desktop\\Apps2Beta1.csv"
+        # path_to_file = "C:\\Users\\wenjli\\Desktop\\Apps2Beta1.csv"
+        path_to_file = "C:\\python37\\Scripts\\myScripts\\Beta_Feedback\\Apps2Beta1.csv"
         data = pd.read_csv(path_to_file, encoding='utf-8')
         data = data.fillna(-1)
         col = ['First L. Name', 'Email Address', 'Hub Satisfaction', 'Hub Comments',
                     'Calendar Satisfaction', 'Calendar Comments', 'Contacts Satisfaction',
                     'Contacts Comments', 'Tasks Satisfaction', 'Tasks Comments',
                     'Notes Satisfaction', 'Notes Comments']
-        for each_col in col:
-            each_col_data = data[each_col]
-            for row in each_col_data:
-                feedback = Feedback()
-                feedback.submitter = row
-                # feedback.email = row['Email Address']
-                #
-                # feedback.score_hub = row['Hub Satisfaction']
-                # feedback.comment_hub = row['Hub Comments']
-                #
-                # feedback.score_calendar = row['Calendar Satisfaction']
-                # feedback.comment_calendar = row['Calendar Comments']
-                #
-                # feedback.score_contacts = row['Contacts Satisfaction']
-                # feedback.comment_contacts = row['Contacts Comments']
-                #
-                # feedback.score_tasks = row['Tasks Satisfaction']
-                # feedback.comment_tasks = row['Tasks Comments']
-                #
-                # feedback.score_notes = row['Notes Satisfaction']
-                # feedback.comment_notes = row['Notes Comments']
 
-                feedback.save()
+        for row in data['Email Address']:
+            feedback = Feedback()
+            feedback.email = row
+            feedback.save()
+        for row in data['First L. Name']:
+            feedback = Feedback()
+            feedback.submitter = row
+            feedback.save()
+
+        # count = 0
+        # for each_col in col:
+        #     each_col_data = data[each_col]
+        #     count += 1
+        #     for row in each_col_data:
+        #         if count == 1:
+        #             feedback = Feedback()
+        #             feedback.submitter = row
+        #             feedback.save()
+        #         elif count == 2:
+        #             feedback = Feedback()
+        #             feedback.email = row
+        #             feedback.save()
+        #     break
 
         if Fruit.objects.exists():
             print('Data already loaded...exiting.')
